562e48a447c42ff65296ee886d80c3de7edd1730
Athul P <141849746+AthulP096@users.noreply.github.com>
2025-07-23 23:27:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function toggleMenu() {
+  var navbar = document.getElementById("navbar");
+  if (navbar.style.right === "0px") {
+    navbar.style.right = "-100%";
+  } else {
+    navbar.style.right = "0";
+  }
+}
+
+var typed = new Typed(".text", {
+  strings: [
+    " Data Analyst",
+    // "Aspiring Machine Learning Engineer",
+    // " Data Science Enthusiast ",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+const circles = document.querySelectorAll(".circle");
+circles.forEach((elem) => {
+  var dots = parseInt(elem.getAttribute("data-dots"));
+  var marked = parseInt(elem.getAttribute("data-percent"));
+  var percent = Math.floor((dots * marked) / 100);
+  var rotate = 360 / dots;
+  let points = "";
+  for (let i = 0; i < dots; i++) {
+    points += `<div class='points' style='--i:${i}; --rot:${rotate}deg'></div>`;
+  }
+  elem.innerHTML = points;
+
+  const pointsMarked = elem.querySelectorAll(".points");
+  for (let i = 0; i < percent; i++) {
+    pointsMarked[i].classList.add("marked");
+  }
+});
